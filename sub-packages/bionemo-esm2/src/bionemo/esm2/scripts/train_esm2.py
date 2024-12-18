40ea637f1eb004d232b44a6975c7fac1be89d13c
sichu <sichu@nvidia.com>
2024-12-18 21:30:28 +0000

    add argparse

--- sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/train_esm2.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/train_esm2.py
@@ -84,6 +84,8 @@ def main(
     save_best_checkpoint: bool = True,
     save_last_checkpoint: bool = True,
     metric_to_monitor_for_checkpoints: str = "val_loss",
+    log_train_ppl: bool = False,
+    log_val_ppl: bool = True,
     save_top_k: int = 2,
     nsys_profiling: bool = False,
     nsys_start_step: int = 0,
@@ -145,6 +147,8 @@ def main(
         save_best_checkpoint (bool): whether to save the best checkpoint
         save_last_checkpoint (bool): whether to save the last checkpoint
         metric_to_monitor_for_checkpoints (str): metric to monitor for checkpoints
+        log_train_ppl (bool): log training perplexity
+        log_val_ppl (bool): log validation perplexity
         save_top_k (int): number of top checkpoints to save
         nsys_profiling (bool): whether to enable nsys profiling
         nsys_start_step (int): start step for nsys profiling
@@ -302,8 +306,8 @@ def main(
             ),
         ),
         # perplexity logging
-        log_train_ppl=False,
-        log_val_ppl=True,
+        log_train_ppl=log_train_ppl,
+        log_val_ppl=log_val_ppl,
     )
 
     # Configure our custom Checkpointer
@@ -387,6 +391,8 @@ def train_esm2_entrypoint():
         save_best_checkpoint=args.save_best_checkpoint,
         save_last_checkpoint=args.save_last_checkpoint,
         metric_to_monitor_for_checkpoints=args.metric_to_monitor_for_checkpoints,
+        log_train_ppl=args.log_train_ppl,
+        log_val_ppl=args.log_val_ppl,
         save_top_k=args.save_top_k,
         nsys_profiling=args.nsys_profiling,
         nsys_start_step=args.nsys_start_step,
@@ -639,6 +645,25 @@ def get_parser():
         required=False,
         default="val_loss",
         help="The metric to monitor for checkpointing.",
+    )
+    parser.add_argument(
+        "--log-train-ppl",
+        action="store_true",
+        default=True,
+        help="Log perplexity during training.",
+    )
+    parser.add_argument(
+        "--log-val-ppl",
+        action="store_true",
+        default=False,
+        help="Log perplexity during validation.",
+    )
+    parser.add_argument(
+        "--no-log-val-ppl",
+        action="store_false",
+        dest="log_val_ppl",
+        default=True,
+        help="Disable logging perplexity during validation.",
     )
     parser.add_argument(
         "--save-top-k",
