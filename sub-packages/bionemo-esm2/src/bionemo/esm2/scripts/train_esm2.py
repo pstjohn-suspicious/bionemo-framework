2ae3d044c7b22f7a992d84102aa99ae66baa0f1a
sichu <sichu@nvidia.com>
2024-12-19 20:26:40 +0000

    remove ppl callback

--- sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/train_esm2.py
+++ sub-packages/bionemo-esm2/src/bionemo/esm2/scripts/train_esm2.py
@@ -214,7 +214,6 @@ def main(
     )
 
     callbacks = [
-        PerplexityLoggingCallback(log_train=False, log_val=True),
         RichModelSummary(max_depth=4),
         LearningRateMonitor(),
         nl_callbacks.PreemptionCallback(),
